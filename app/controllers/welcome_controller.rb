ef740b07ffb838ef7f6ab645290aa220a087a960
ManishArora93 <manisharora454@gmail.com>
2018-09-30 17:33:44 +0530

    Added slim pages and devise for authentication

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,17 @@
+class WelcomeController < ApplicationController
+  def index
+    @string_to_be_displayed = "Hello World"
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  resources :articles
+  get 'welcome/index'
+  get 'welcome/update'
+  get 'welcome/edit'
+  get 'welcome/show'
+  get 'welcome/create'
+  root 'welcome#index'
+
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
